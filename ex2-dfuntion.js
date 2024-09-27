1cbec717482459ba33323af5b7e76801893d6944
Med-Zato <132960997+Med-Zato@users.noreply.github.com>
2024-09-27 20:26:02 +0100

    Updated ex2 file from user2 by changing b = 100 and c = 200

--- ex2-dfuntion.js
+++ ex2-dfuntion.js
@@ -3,3 +3,7 @@ function multiply(n) {
 }
 console.log(52);
 console.log("Added this line from the github website for the first time");
+console.log("Added this line from user2 account");
+var b = 100;
+var c = 200;
+
